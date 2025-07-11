50982f805371ac6861cf613d50aadc5843e0004c
shystera <120552322+shystera@users.noreply.github.com>
2025-07-11 18:59:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,25 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve('.'),
         }
+      },
+      build: {
+        rollupOptions: {
+          input: {
+            main: path.resolve(__dirname, 'index.html'),
+            indexOne: path.resolve(__dirname, 'index-1.html'),
+            buildCourse: path.resolve(__dirname, 'build-course.html'),
+            coachAdminPanel: path.resolve(__dirname, 'coach-admin-panel.html'),
+            courseDetail: path.resolve(__dirname, 'course-detail.html'),
+            createCourse: path.resolve(__dirname, 'create-course.html'),
+            editCourse: path.resolve(__dirname, 'edit-course.html'),
+            lessonPlayer: path.resolve(__dirname, 'lesson-player.html'),
+            login: path.resolve(__dirname, 'login.html'),
+            role: path.resolve(__dirname, 'role.html'),
+            signup: path.resolve(__dirname, 'signup.html'),
+            studentPanel: path.resolve(__dirname, 'student-panel.html'),
+            video: path.resolve(__dirname, 'video.html'),
+          }
+        }
       }
     };
 });
